77ac03bdeb2357bef5849733ad38e5f6f72d10b7
qinglu.dxw <qinglu.dxw@raycloud.com>
2021-09-14 09:00:05 +0800

    feat: 修改 cleanObject 参数类型，以及 bug 修复 by dxw

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,19 @@
 import { useEffect, useState } from 'react';
 
 export const isFalsy = (param: unknown) => (param === 0 ? false : !param);
+export const isVoid = (value: unknown) =>
+    value === undefined || value === null || value === '';
 
 /**
  * @description 清除对象上没有值的键
  * @param object 需要清理的对象
  * @returns 清理完成的对象
  */
-export const cleanObject = (object: object) => {
+export const cleanObject = (object: { [key: string]: string }) => {
     const result = { ...object };
     Object.keys(result).forEach((key) => {
-        // @ts-ignore
         const value = result[key];
-        if (isFalsy(value)) {
-            // @ts-ignore
+        if (isVoid(value)) {
             delete result[key];
         }
     });
